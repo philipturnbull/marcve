6316fbd7c1bfc0ffe2300a4d8b1f021b0f74518a
Phil Turnbull <philip.turnbull@gmail.com>
2017-01-21 12:27:19 -0500

    Use an order-2 chain.

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Generate random CVE decscriptions with Markov chains:
 
     $ curl https://cve.mitre.org/data/downloads/allitems.xml.gz | gunzip > allitems.xml
     $ marcve --filename allitems.xml &
-    $ curl http://127.0.0.1:8080/cve/2020/1471
-    Unspecified vulnerability in Cisco Secure Elements Class Name field, which
-    allows remote attackers to obtain sensitive environment variable and 2.2.x
-    before 9.5.2 and 3.x, allows remote attackers to execute arbitrary web cache
-    poisoning attacks via the network traffic after a URL in Linux SCTP data.
+    $ curl http://127.0.0.1:8080/cve/2019/1531
+    Cross-site scripting vulnerability in the sourceAFRICA plugin 0.1.3 for
+    WordPress allows remote attackers to bypass intended sandbox restrictions via a
+    crafted app that leverages improper handling of O_DIRECT (direct IO) write
+    requests.
--- pkg/markov/markov.go
+++ pkg/markov/markov.go
@@ -9,6 +9,31 @@ type Markov struct {
 	entries map[string][]string
 }
 
+type Prefix struct {
+	words [2]string
+}
+
+func (p *Prefix) key() string {
+	if len(p.words[0]) > 0 && len(p.words[1]) > 0 {
+		return p.words[0] + " " + p.words[1]
+	} else if len(p.words[0]) > 0 {
+		return p.words[0]
+	} else {
+		return ""
+	}
+}
+
+func (p *Prefix) push(word string) {
+	if len(p.words[0]) > 0 && len(p.words[1]) > 0 {
+		p.words[0] = p.words[1]
+		p.words[1] = word
+	} else if len(p.words[0]) > 0 {
+		p.words[1] = word
+	} else {
+		p.words[0] = word
+	}
+}
+
 const (
 	start_of_string = ""
 	end_of_string = "\n"
@@ -19,20 +44,25 @@ func New() *Markov {
 }
 
 func (markov *Markov) Update(s string) {
-	var prev string = start_of_string
+	var prefix Prefix
 	for _, word := range strings.Split(s, " ") {
-		markov.entries[prev] = append(markov.entries[prev], word)
-		prev = word
+		key := prefix.key()
+		markov.entries[key] = append(markov.entries[key], word)
+		prefix.push(word)
 	}
-	markov.entries[prev] = append(markov.entries[prev], end_of_string)
+
+	key := prefix.key()
+	markov.entries[key] = append(markov.entries[key], end_of_string)
 }
 
 func (markov *Markov) Generate(r *rand.Rand) string {
 	var output = []string{}
 	var word = start_of_string
 
+	var prefix Prefix
 	for {
-		entry := markov.entries[word]
+		key := prefix.key()
+		entry := markov.entries[key]
 		word = entry[r.Intn(len(entry))]
 
 		if word == end_of_string {
@@ -40,6 +70,7 @@ func (markov *Markov) Generate(r *rand.Rand) string {
 		}
 
 		output = append(output, word)
+		prefix.push(word)
 	}
 	return strings.Join(output, " ")
 }
